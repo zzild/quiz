25f9414cc3bf2e47736e746701e82408d634743d
zild <zild@mymble.moominland>
2016-10-24 17:59:44 +0200

    more stuff

--- Quiz (1o1).py
+++ Quiz (1o1).py
@@ -79,3 +79,55 @@ for i in range (1,40):
 print("Deine Antwort",Player2)
 Frage32 = input()
 print("Die Franzoesin Jeanne Calment ist 122 Jahre alt geworden")
+Frage311 = int(Frage31) - 122
+Frage321 = int(Frage32) - 122
+antwort31 = int(Frage311) * int(Frage311)
+antwort32 = int(Frage321) * int(Frage321)
+if antwort31 < antwort32:
+    print("Damit geht der Punkt an",Player1)
+    Score1 = Score1 + 1
+if antwort31 > antwort32:
+    print("Damit gewinnt",Player2)
+    Score2 = Score2 + 1
+if antwort31 == antwort32:
+    print("kein Punkt fuer irgendwen!")
+print("Jetzt kommt Frage 4:")
+print("Wie viele Alben haben die Beatles veroeffentlicht?")
+print(Player1,"?")
+Frage41 = input()
+for i in range (1,40):
+    print ("don't look")
+print (Player2,"?")
+Frage42 = input()
+print("Die Beatles haben insgesamt 13 Alben veroeffentlich")
+Frage411=int(Frage41) - 13
+Frage421=int(Frage42) - 13
+antwort41 = int(Frage411) * int(Frage411)
+antwort42 = int(Frage421) * int(Frage421)
+if antwort41 < antwort42:
+    print ("Der Punkt geht somit an", Player1)
+    Score1 = Score1 + 1
+if antwort41 > antwort42:
+    print ("Der Punkt geht somit an",Player2)
+    Score2 = Score2 + 1
+if antwort41== antwort42:
+    print("Kein Punkt")
+print("Kommen wir zur naechsten Frage")
+print("Wie viele Staffeln Simpsons gibt es aktuell?")
+print(Player1,"?")
+Frage51 = input()
+for i in range (1,40):
+    print ("don't look")
+Frage52 = (input)
+print("Es gibt aktuell 27 Staffeln der Simpsons")
+Frage511 = int(Frage51) - 27
+Frage521 = int(Frage52) - 27
+antwort51 = int(Frage511) * int(Frage511)
+antwort52 = int(Frage521) * int(Frage521)
+if antwort51 < antwort52
+    print("Der Punkt geht somit an ",Player1)
+    Score1 = Score1 + 1
+if antwort51 > antwort52
+    print("Der Punkt geht somit an", Player2)
+    Score2 = Score2 + 1
+
