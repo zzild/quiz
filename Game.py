8993f1ee9780475236a7222f864467f4d2d8af78
zild <kater@meyling.com>
2016-11-06 13:31:35 +0100

    programm laueft durch, aber nur mit dummies

--- Game.py
+++ Game.py
@@ -13,27 +13,34 @@ def begruessung():
 # wartet bis zeit zuende oder eingabe kommt
 # liefert die eingabe oder "nichts" zurueck
 def input_innerhalb_zeit():
-    return "a"
+    return input()
 
+#Liefert ok zueruck wenn nicht nichts
 def test(antwort):
     if antwort == "a":
         antwort = "ok"
+    else:
+        antwort = ""
+    return antwort
 
 
 
 # spielt das spiel und veraendert score
 def spiel():
+    global score
     s = "true"
     print("go go")
 
     print("wenn das wort in der ersten haelfte des alphabetes ist schreibe a wenn nicht dann b")
     while  s == "true":
-        dingart = random()
+        zufbu = random()
         antwort = input_innerhalb_zeit()
-        test(antwort)
+        antwort = test(antwort)
         if antwort == "ok":
             s = "true"
             score = score + 1
+        else:
+            break
     return
 
 def ende():
